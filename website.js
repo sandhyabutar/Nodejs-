6ba0e0b6891c8780e8e2f6223d073363f5fd652d
sandhyabutar <india.mymail@gmail.com>
2022-08-26 11:48:31 +0530

    update index.html &website.js

--- website.js
+++ website.js
@@ -1,33 +1,19 @@
- const {Console}= require('console');
-const fs = require('fs');
+const {Console} = require('console');
 const http = require('http');
 
 const port = process.env.PORT || 3000  ;
 
 const server = http.createServer((req, res)=>{
-  res.setHeader('Content-Type','text/html')
-    console.log(req.url)
-    if(req.url='/abc')
-    {
-      res.statusCode = 200;
-      
-      
-    }
-  else if(req.url = '/about')
-  {
-    res.statusCode = 200;
-    res.end('<h1>About Code With Sandhya</h1><p>Hey this is the About code with ButarGirl!</p>');
-  } 
-  else if(req.url ='/hello'){
-    res.statusCode = 200;
-    const data = fs.readFileSync('index.html');
-    res.end(data.toString());
-  }
    
-  else{
-    res.statusCode = 404; 
-    res.end('<h1>Not found</h1><p>This page is not found!</p>')
-  }
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html')
+    console.log(req.url)
+    if(req.url == '/'){
+    res.end('<h1>This is Code With Butar girl</h1><p>Hey this is the way to rock the world!</p>');
+    }
+    else if(req.url == '/about'){
+      res.end('<h1>About Code With Sandhya</h1><p>Hey this is the way to rock the world!</p>');
+    }
 })
 server.listen(port, ()=>{
 console.log('Server is listing on port ${port}')
